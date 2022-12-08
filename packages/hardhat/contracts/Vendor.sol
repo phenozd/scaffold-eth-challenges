7b1991fca710a53daa8458241e92e401240839fd
phenozd <danielnsolomon@gmail.com>
2022-12-07 22:04:43 -0500

    challenge 2

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -6,12 +6,23 @@ import "./YourToken.sol";
 
 contract Vendor is Ownable {
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
   YourToken public yourToken;
 
   constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
+  }
+  
+  uint256 public constant tokensPerEth = 100;
+  
+  function buyTokens() public payable {
+	  yourToken.transfer(msg.sender, msg.value * tokensPerEth);
+	  emit BuyTokens(msg.sender, msg.value, msg.value * tokensPerEth);
+  }
+  
+  function withdraw() public onlyOwner {
+	(bool sent,) = msg.sender.call{value: address(this).balance}("");
   }
 
   // ToDo: create a payable buyTokens() function:
@@ -19,5 +30,11 @@ contract Vendor is Ownable {
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
 
   // ToDo: create a sellTokens(uint256 _amount) function:
+  
+  function sellTokens(uint256 _amount) public {
+	  yourToken.transferFrom(msg.sender, address(this), _amount);
+	  (bool success,) = msg.sender.call{value: _amount / tokensPerEth}("");
+	  require(success, "Failed to send Ether");
+  }
 
 }
